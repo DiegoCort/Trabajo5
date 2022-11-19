992c3f4a3cc88cb1982ec9dab5068a9388eebbd9
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-11-19 11:06:07 -0500

    Update Notas.java

--- Notas/src/notas/Notas.java
+++ Notas/src/notas/Notas.java
@@ -1,20 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package notas;
 
-/**
- *
- * @author 57324
- */
 public class Notas {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    //Array de notas
+    double [] ListaNotas; 
+    
+    /**se crea el constructor notas y se le da 
+    tamaño al array
+    */
+    public Notas(){
+        ListaNotas = new double[5];
     }
     
+    /** Metodo para calcular el promedio de 
+    las notas y se optiene mediante un return
+     */
+    double cPromedio(){
+        double suma = 0;
+        for(int i=0;i<ListaNotas.length;i++){
+            suma+=ListaNotas[i];
+        }
+        return(suma/ListaNotas.length);
+    }
+    
+    /**Metodo para calcular la desviacion estandar 
+    del array de notas mediante un return
+     */
+    double dEstandar(){
+        double promedio = cPromedio();//se calcula el promedio mediante el metodo
+        double suma = 0;
+        for(int i=0;i<ListaNotas.length;i++){
+            suma+=Math.pow(ListaNotas[i]-promedio, 2);//sumatoria de alementos
+        }
+        return Math.sqrt(suma/ListaNotas.length);//calculo de la desviacion
+    }
+    
+    /**Metodo para calcular el valor menor de las 
+    notas en el array mediante el return 
+     */
+    double cMenor(){
+        double menor = ListaNotas[0];//Se crea una variable como la nota menor
+        for(int i = 0;i<ListaNotas.length;i++){
+            /**
+            se compara la variable con las notas de array
+            * y si alguna es menor se actualiza
+             */
+            if(ListaNotas[i]<menor){;
+                menor=ListaNotas[i];
+            }
+        }
+        return menor;
+    }
 }
