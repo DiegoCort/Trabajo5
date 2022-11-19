7dd4c88e34131908f227b6505ec00acdb828c4de
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-11-19 12:53:32 -0500

    Create Principal.java

---
+++ Notas/src/notas/Principal.java
@@ -0,0 +1,13 @@
+
+package notas;
+
+
+public class Principal {
+
+    public static void main(String[] args){
+        
+        ventanaPrincipal miventanaPrincipal;
+        miventanaPrincipal = new ventanaPrincipal();
+        miventanaPrincipal.setVisible(true);
+    }
+}
