29790c5d8f009bf509a2ffa668bd0fadd0f0bed2
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-11-19 23:19:40 -0500

    Create Voltaje.java

---
+++ AppMenus/src/universidadnacional/Voltaje.java
@@ -0,0 +1,12 @@
+
+package universidadnacional;
+
+public class Voltaje {
+    
+    public static double calcularVoltaje(double corriente, double resistencia){
+        double voltaje;
+        voltaje = corriente*resistencia;
+        return voltaje;
+    }
+    
+}
