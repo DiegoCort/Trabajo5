4af21ad4971313d3346b0accb331654c66d160a7
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-11-19 23:19:07 -0500

    Create Corriente.java

---
+++ AppMenus/src/universidadnacional/Corriente.java
@@ -0,0 +1,12 @@
+
+package universidadnacional;
+
+public class Corriente {
+    
+    public static double calcularCorriente(double voltaje, double resistencia){
+        double Corriente;
+        Corriente = voltaje/resistencia;
+        return Corriente;
+    }
+    
+}
