5d55ede4e52642b1dcfdc1021f4e6a404e4dc969
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-11-19 23:19:38 -0500

    Create Pitagoras.java

---
+++ AppMenus/src/universidadnacional/Pitagoras.java
@@ -0,0 +1,11 @@
+
+package universidadnacional;
+
+public class Pitagoras {
+    
+    public static double calcularHipotenusa(double catetoA, double catetoB){
+        double hipotenusa;
+        hipotenusa = Math.sqrt(Math.pow(catetoB, 2) + Math.pow(catetoB, 2));
+        return hipotenusa;
+    }
+}
