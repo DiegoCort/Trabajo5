edb245db16e1017b8986e8d7562f739fcbd0bafe
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-11-19 21:42:03 -0500

    Create UniversidadNacional.java

---
+++ AppMenus/src/universidadnacional/UniversidadNacional.java
@@ -0,0 +1,13 @@
+
+package universidadnacional;
+
+public class UniversidadNacional {
+
+
+    public static void main(String[] args) {
+      
+    fmrPrincipal principal = new fmrPrincipal();
+    principal.setVisible(true);
+    }
+    
+}
